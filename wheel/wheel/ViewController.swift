f329e5b1be42324745e984c98bc6924532b68371
qazx.mac <qazx.mac@gmail.com>
2024-02-29 08:01:38 +0700

    no message

---
+++ wheel/wheel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wheel
+//
+//  Created by admin on 29/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
